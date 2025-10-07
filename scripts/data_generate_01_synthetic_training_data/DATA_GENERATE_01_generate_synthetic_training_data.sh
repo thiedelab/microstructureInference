fe591b9e7760c8e62be2efcbc9cb794ad4c31989
Kwanghwi Je <jekw@umich.edu>
2025-10-07 09:11:30 -0400

    updated README.md

--- scripts/data_generate_01_synthetic_training_data/DATA_GENERATE_01_generate_synthetic_training_data.sh
+++ scripts/data_generate_01_synthetic_training_data/DATA_GENERATE_01_generate_synthetic_training_data.sh
@@ -6,8 +6,11 @@ source /home/kwang/anaconda3/etc/profile.d/conda.sh
 ## Activate your desired Conda environment
 conda activate py4DSTEM14.08-envforPlot
 
-echo "Make sure bash files are executable"
-echo "chmod +x STEP_01_sample_orientation_thickness.sh STEP_02_simulate_dynamic_diffraction.sh STEP_03_digitize_dynamic_diffraction.sh STEP_04_merge_and_split.sh"
+echo "Make sure bash files in ./scripts/data_generate_01_synthetic_training_data are executable"
+echo "chmod +x ./scripts/data_generate_01_synthetic_training_data/STEP_01_sample_orientation_thickness.sh"
+echo "chmod +x ./scripts/data_generate_01_synthetic_training_data/STEP_02_simulate_dynamic_diffraction.sh STEP_03_digitize_dynamic_diffraction.sh STEP_04_merge_and_split.sh"
+echo "chmod +x ./scripts/data_generate_01_synthetic_training_data/STEP_03_digitize_dynamic_diffraction.sh STEP_04_merge_and_split.sh"
+echo "chmod +x ./scripts/data_generate_01_synthetic_training_data/STEP_04_merge_and_split.sh"
 
 echo "STEP_01_sample_orientation_thickness.sh"
 ./scripts/data_generate_01_synthetic_training_data/STEP_01_sample_orientation_thickness.sh
