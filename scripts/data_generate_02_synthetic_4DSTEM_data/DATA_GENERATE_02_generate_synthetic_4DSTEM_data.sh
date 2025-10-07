b0eee52810d3662922442a16743d04bbc2deb308
Kwanghwi Je <jekw@umich.edu>
2025-10-07 09:13:26 -0400

    updated README.md

--- scripts/data_generate_02_synthetic_4DSTEM_data/DATA_GENERATE_02_generate_synthetic_4DSTEM_data.sh
+++ scripts/data_generate_02_synthetic_4DSTEM_data/DATA_GENERATE_02_generate_synthetic_4DSTEM_data.sh
@@ -6,7 +6,7 @@ source /home/kwang/anaconda3/etc/profile.d/conda.sh
 ## Activate your desired Conda environment
 conda activate py4DSTEM14.08-envforPlot
 
-echo "Make sure bash files in './scripts/data_generate_01_synthetic_training_data' are executable"
+echo "Make sure bash files in './scripts/data_generate_02_synthetic_4DSTEM_data' are executable"
 echo "chmod +x ./scripts/data_generate_01_synthetic_training_data/STEP_01_sample_orientation_thickness.sh"
 echo "chmod +x ./scripts/data_generate_01_synthetic_training_data/STEP_02_simulate_dynamic_diffraction.sh STEP_03_digitize_dynamic_diffraction.sh STEP_04_merge_and_split.sh"
 echo "chmod +x ./scripts/data_generate_01_synthetic_training_data/STEP_03_digitize_dynamic_diffraction.sh STEP_04_merge_and_split.sh"
